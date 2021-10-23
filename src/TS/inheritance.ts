da9334e27ac0d0f3283ee185069c1bf8b72419a5
BeWoler <bewoler.del@gmail.com>
2021-10-23 21:09:52 +0300

    created tsconfig & added inheritance.ts

---
+++ src/TS/inheritance.ts
@@ -0,0 +1,50 @@
+class Person {
+    private _firstName;
+    private _lastName;
+    private _age;
+
+    constructor(firstName, lastName, age) {
+        this._firstName = firstName;
+        this._lastName = lastName;
+        this._age = age;
+    }
+
+    public get firstName() {
+        return this._firstName
+    }
+
+    public set firstName(value) {
+        this._firstName = value
+    }
+
+    public get lastName() {
+        return this._lastName
+    }
+
+    public set lastName(value) {
+        this._lastName = value
+    }
+
+    public get age() {
+        return this._age
+    }
+
+    public set age(value) {
+        value < 0 ? this._age = 0 : this._age = value; 
+    }
+}
+
+class Employee extends Person {
+    private _inn;
+    private _number;
+    private _snils
+
+    constructor(firstName, lastName, age, inn, number, snils) {
+        super(firstName, lastName, age);
+        this._inn = inn;
+        this._number = number;
+        this._snils = snils;
+    }
+}
+
+const employee1 = new Employee('Stanislav', 'Vorozhun', 'dont remember', 15, 20, 30);
